7a23c47deb99422f480e167b13b8cdcb86642d27
Wiktor Dudzinski <viktor.dudzinski@gmail.com>
2019-08-19 22:01:58 +0200

    modele_bez_relacji

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    description = models.TextField
+
+
+class Phone(models.Model):
+    number = models.IntegerField
+    type = models.CharField(max_length=64)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=64)
+    street = models.CharField(max_length=64)
+    n_house = models.IntegerField
+    n_flat = models.IntegerField
+
+
+class Email(models.Model):
+    email = models.CharField(max_length=128)
+    type = models.CharField(max_length=64)
 # Create your models here.
+#miasto, ulicę, numer domu, numer mieszkania
